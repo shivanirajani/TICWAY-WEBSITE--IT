25cb9b9a6465b6a1d01a258cdf086693029b51f7
shivanirajani <c1015654@newcastle.ac.uk>
2024-05-10 16:15:38 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Specify the output configuration
+
     output: 'export',
     
-    // Configure images to disable the Image Optimization API
+
     images: {
       unoptimized: true
     }
